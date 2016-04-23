ff101e384cc44777ffdda23852be19e44cfb02fb
Nikolaos Vlassopoulos <nvlassopoulos@gmail.com>
2016-04-23 11:44:03 +0200

    initial commit

---
+++ project.clj
@@ -0,0 +1,31 @@
+(defproject nbg_cbank "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.draines/postal "1.11.3"]
+                 [honeysql "0.6.3"]
+                 [org.clojure/java.jdbc "0.5.8"]
+                 [com.stuartsierra/component "0.3.1"]
+                 ;;[mount "0.1.10"]
+                 [compojure "1.5.0"]
+                 [http-kit "2.1.18"]
+                 [ring/ring-json "0.4.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 ;;
+                 [buddy/buddy-core "0.12.0"]
+                 [buddy/buddy-auth "0.12.0"]
+                 [buddy/buddy-sign "0.12.0"]
+                 [com.zaxxer/HikariCP "2.4.5"]
+                 ;;
+                 [org.postgresql/postgresql "9.4.1208.jre7"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 ;;[org.apache.logging.log4j/log4j-core "2.5"]]
+                 [org.slf4j/slf4j-api "1.7.12"]
+                 [org.slf4j/slf4j-log4j12 "1.7.12"]
+                 [org.clojure/core.async "0.2.374"]]
+
+  :main ^:skip-aot nbg-cbank.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ src/nbg_cbank/core.clj
@@ -0,0 +1,112 @@
+(ns nbg-cbank.core
+  (:require [com.stuartsierra.component :as mc])
+  (:require [compojure.core :as cc]
+            [compojure.route :as cr])
+  (:require [org.httpkit.server :as wsrv])
+  (:require [postal.core :as pc])
+  (:require [honeysql.core :as hc]
+            [honeysql.format :as hf])
+  (:require [clojure.java.jdbc :as j])
+  (:require [clojure.tools.logging :as log])
+  (:require [clojure.core.async :as asnc])
+  (:import [com.zaxxer.hikari HikariConfig
+            HikariDataSource])
+  (:gen-class))
+
+;; (pc/send-message {:host "localhost"}
+;;                  {:from "nvlass@megacorp1.eu"
+;;                   :to "giorgospapa86@gmail.com"
+;;                   :subject "Testing Clojure email subsystems #2"
+;;                   :body "Should you receive this email, then all systems are nominal"})
+
+(defn make-hikari-datasource [props]
+  (let [hc (HikariConfig. (:hikari-props props))]
+    (HikariDataSource.  hc)))
+
+(defn stop-hikari-datasource [ds]
+  (.close ds))
+
+
+;; (defn ws-handler [req]
+;;   (wsrv/with-channel req chan
+;;     (wsrv/on-close chan (fn [status] (log/info "channel closed: " status)))
+;;     (wsrv/on-receive chan (fn [data]
+;;                             (log/info "received: " data)
+;;                             (wsrv/send! chan data)))))
+
+(cc/defroutes cbank-routes
+  (cc/GET "/" [] {:status 200
+                  :body "the empty route"})
+  (cc/GET "/projects" {:status 200
+                       :body "asd"}))
+
+  ;;(cc/GET "/wsapi/" [] ws-handler))
+
+
+(defn start-webserver [prop-map]
+  (wsrv/run-server #'cbank-routes
+                   {:port (:port prop-map)
+                    :ip (:ip prop-map)
+                    :thread (:thread prop-map)}))
+
+(defn stop-webserver [stop-fn]
+  (stop-fn))
+
+
+(defrecord db [conf connection]
+  mc/Lifecycle
+  (start [component]
+    (log/info "starting database")
+    (when (nil? (:connection component))
+      (let [conn (make-hikari-datasource conf)]
+        (assoc component :connection conn))))
+  (stop [component]
+    (log/info "stopping database")
+    (when-not (nil? (:connection component))
+      (.close connection)
+      (assoc component :connection nil))))
+
+
+(defrecord ws [conf ws]
+  mc/Lifecycle
+  (start [component]
+    (log/info "starting webserver")
+    (when (nil? (:ws component))
+      (assoc component :ws (start-webserver conf))))
+  (stop [component]
+    (log/info "stopping webserver")
+    (when-not (nil? (:ws component))
+      (let [stopfn (:ws component)]
+        (stopfn)
+        (assoc component :ws nil)))))
+
+(defn new-db [conf]
+  (map->db {:conf conf}))
+
+(defn new-ws [conf]
+  (map->ws {:conf conf}))
+
+(defn make-system [conf]
+  (mc/system-map
+   :db (new-db conf)
+   :ws (mc/using
+        (new-ws conf)
+        [:db])))
+
+(def app-conf {:hikari-props "resources/hikari.properties"
+               :port 8989
+               :ip "127.0.0.1"
+               :thread 4})
+
+(def system (make-system app-conf))
+
+(defn start-system []
+  (alter-var-root #'system mc/start))
+
+(defn stop-system []
+  (alter-var-root #'system mc/stop))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
